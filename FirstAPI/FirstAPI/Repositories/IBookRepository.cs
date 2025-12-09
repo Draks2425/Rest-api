e5ab907b22348d99ccbe127f97a0e9aa5dc386e6
Kacper Kraus <kacperkraus2425@gmail.com>
2025-12-09 15:03:32 +0100

    Create IBookRepository interface defining CRUD operations

---
+++ FirstAPI/FirstAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,23 @@
+﻿using FirstAPI.Models;
+
+namespace FirstAPI.Repositories
+{
+    // Interface defining the operations for Book data access
+    public interface IBookRepository
+    {
+        // Get all books
+        Task<IEnumerable<Book>> Get();
+
+        // Get a specific book by its ID
+        Task<Book?> GetById(int id);
+
+        // Add a new book to the database
+        Task<Book> Add(Book book);
+
+        // Update an existing book
+        Task Update(Book book);
+
+        // Delete a book by its ID
+        Task Delete(int id);
+    }
+}
